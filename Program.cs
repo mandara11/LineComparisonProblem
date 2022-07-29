a557d6e72fa8397b26b4a7627304a802e23b1129
mandara11 <mandaram2345@gmail.com>
2022-07-29 19:36:41 +0530

    AddedUC1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LineComparisonProgram");
-            Length.LineLength();    //UC1
+            //Length.LineLength();    //UC1
+            LineCompare.IsEqul();   //UC2
         }
     }
 }
